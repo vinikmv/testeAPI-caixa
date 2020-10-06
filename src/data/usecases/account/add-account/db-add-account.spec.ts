0ad8d01053dc60460fb919df690ae39ff316442f
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-06 12:24:02 -0300

    test: ensure DbAddAccount throw if AddAccountRepository throws

--- src/data/usecases/account/add-account/db-add-account.spec.ts
+++ src/data/usecases/account/add-account/db-add-account.spec.ts
@@ -77,4 +77,15 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password'
     })
   })
+
+  test('Should throw if AddAccountRepository throws ', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accoundData = {
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accoundData)
+    await expect(promise).rejects.toThrow()
+  })
 })
