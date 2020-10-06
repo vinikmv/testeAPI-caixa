5116f58bfb99e1e493e85825b5332d53a4a8a7dd
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-06 08:33:29 -0300

    test: ensure SignUpController returns an error if no param if provided

---
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -0,0 +1,31 @@
+import faker from 'faker'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { SignUpController } from './signup-controller'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no email if provided', () => {
+    const sut = new SignUpController()
+    const password = faker.internet.password()
+    const httpRequest = {
+      body: {
+        password,
+        passwordConfirmation: password
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if no password if provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: faker.internet.email()
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+})
