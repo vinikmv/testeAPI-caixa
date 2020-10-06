99ec27488c5211cc860bafcb35f7afa180605657
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-06 13:52:35 -0300

    feat: ensure AccountMongoRepository return an account on success

---
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -0,0 +1,14 @@
+import { AddAccountRepository } from '@/data/protocols/db/add-account/db-add-account-repository'
+import { AccountModel } from '@/domain/models/account'
+import { AddAccountParams } from '@/domain/usecases/account/add-account'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountParams): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = result.ops[0]
+    const { _id, ...accountWithoutId } = account
+    return { ...accountWithoutId, id: _id }
+  }
+}
