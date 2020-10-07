6c05e4d5c03da0d343f3c07e4a5be6636c59d254
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-07 14:13:22 -0300

    chore: fix path imports

--- src/main/factories/controllers/login/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login/login-controller-factory.ts
@@ -1,5 +1,5 @@
 
-import { makeLoginValidation } from '@/main/factories/controllers/login/login-validation-factory'
+import { makeLoginValidation } from '@/main/factories/controllers/login/login/login-validation-factory'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { makeDbAuthentication } from '@/main/factories/usecases/authentication/db-authentication-factory'
 import { LoginController } from '@/presentation/controllers/login/login/login-controller'
