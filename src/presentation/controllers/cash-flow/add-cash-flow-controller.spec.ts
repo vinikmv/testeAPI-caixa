b617c1ab8d0ada62515dc90c2cf923a137b9e48e
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-07 12:13:56 -0300

    test: ensure CashFlowController return 400 if Validation fails

--- src/presentation/controllers/cash-flow/add-cash-flow-controller.spec.ts
+++ src/presentation/controllers/cash-flow/add-cash-flow-controller.spec.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Validation } from '@/presentation/protocols'
 import { AddCashFlowController } from './add-cash-flow-controller'
 import { HttpRequest } from './add-cash-flow-protocols'
@@ -44,5 +45,12 @@ describe('CashFlow Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
