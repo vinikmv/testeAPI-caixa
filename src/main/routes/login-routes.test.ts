1e2c79fb9fb11f4b9703e863c36cd4b729da51b2
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-07 09:51:39 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,7 @@
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '@/main/config/app'
 import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 import request from 'supertest'
 
 let accountCollection: Collection
@@ -27,6 +28,23 @@ describe('Login Routes', () => {
           email: 'vinicius_teste@gmail.com',
           password: '12345',
           passwordConfirmation: '12345'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return an account on success ', async () => {
+      const password = await hash('12345', 12)
+      await accountCollection.insertOne({
+        email: 'vinicius_teste@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'vinicius_teste@gmail.com',
+          password: '12345'
         })
         .expect(200)
     })
