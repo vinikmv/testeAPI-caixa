762dd3a1e165b2043d76e2cac86aa9e9967eb00e
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-07 13:26:47 -0300

    test: ensure DbAddCashFlow calls AddCashFlowRepository with correct values

---
+++ src/data/usecases/add-cash-flow/db-add-cash-flow.spec.ts
@@ -0,0 +1,45 @@
+import { AddCashFlowRepository, AddCashFlowParams } from './db-add-cash-flow-protocols'
+import { DbAddCashFlow } from '@/data/usecases/add-cash-flow/db-add-cash-flow'
+import faker from 'faker'
+
+const makeAddCashFlowRepositoryStub = (): AddCashFlowRepository => {
+  class AddCashFlowRepositoryStub implements AddCashFlowRepository {
+    async add (cashData: AddCashFlowParams): Promise<void> {
+      return await Promise.resolve()
+    }
+  }
+  return new AddCashFlowRepositoryStub()
+}
+
+const makeFakeCashData = (): AddCashFlowParams => ({
+  categoria: {
+    name: 'any_name'
+  },
+  tipo: 'any_tipo',
+  valor: faker.random.number(),
+  descricao: 'any_descricao'
+})
+
+interface SutTypes {
+  sut: DbAddCashFlow
+  addCashFlowRepositoryStub: AddCashFlowRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addCashFlowRepositoryStub = makeAddCashFlowRepositoryStub()
+  const sut = new DbAddCashFlow(addCashFlowRepositoryStub)
+  return {
+    sut,
+    addCashFlowRepositoryStub
+  }
+}
+
+describe('DbAddCashFlow Usecase', () => {
+  test('Should call AddCashFlowRepository with correct values ', async () => {
+    const { sut, addCashFlowRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addCashFlowRepositoryStub, 'add')
+    const cashFlowData = makeFakeCashData()
+    await sut.add(cashFlowData)
+    expect(addSpy).toHaveBeenCalledWith(cashFlowData)
+  })
+})
