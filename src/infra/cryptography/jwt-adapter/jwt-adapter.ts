96efe180f0a07686573984e51c0f6743c11ae1c8
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-07 16:28:51 -0300

    feat: ensure JwtAdapter implements Decrypter correctly

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken'
 import { Encrypter } from '@/data/protocols/cryptography/encrypter'
+import { Decrypter } from '@/data/protocols/cryptography/decrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (
     private readonly secret: string
   ) {}
@@ -9,5 +10,10 @@ export class JwtAdapter implements Encrypter {
   async encrypt (value: string): Promise<string> {
     const accessToken = jwt.sign({ id: value }, this.secret)
     return accessToken
+  }
+
+  async decrypt (token: string): Promise<string> {
+    const value: any = jwt.verify(token, this.secret)
+    return value
   }
 }
