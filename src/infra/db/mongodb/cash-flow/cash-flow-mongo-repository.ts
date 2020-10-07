83e9ffde10d9ed027fbe605f8ab8dcf1d69076f3
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-07 14:50:42 -0300

    refactor: insert id in categoria  when create cashFlowData

--- src/infra/db/mongodb/cash-flow/cash-flow-mongo-repository.ts
+++ src/infra/db/mongodb/cash-flow/cash-flow-mongo-repository.ts
@@ -1,10 +1,13 @@
 import { AddCashFlowRepository } from '@/data/protocols/db/cash-flow/add-cash-flow-repository'
 import { AddCashFlowParams } from '@/domain/usecases/cash-flow/add-cash-flow'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class CashFlowMongoRepository implements AddCashFlowRepository {
   async add (cashData: AddCashFlowParams): Promise<void> {
     const cashFlowCollection = await MongoHelper.getCollection('cashflows')
-    await cashFlowCollection.insertOne(cashData)
+    const { categoria, ...cashDataSemCategoria } = cashData
+    const { name } = categoria
+    await cashFlowCollection.insertOne({ ...cashDataSemCategoria, categoria: { id: new ObjectId(), name } })
   }
 }
