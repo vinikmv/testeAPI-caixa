208de89bd2c0bb3109fff91bbb10eb45edd94307
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-08 15:05:04 -0300

    feat: ensure CashFlowMongoRepository load all cash flow on success

--- src/infra/db/mongodb/cash-flow/cash-flow-mongo-repository.ts
+++ src/infra/db/mongodb/cash-flow/cash-flow-mongo-repository.ts
@@ -1,13 +1,21 @@
 import { AddCashFlowRepository } from '@/data/protocols/db/cash-flow/add-cash-flow-repository'
+import { LoadCashFlowRepository } from '@/data/protocols/db/cash-flow/load-cash-flow-repository'
+import { CashFlowModel } from '@/domain/models/cash-flow'
 import { AddCashFlowParams } from '@/domain/usecases/cash-flow/add-cash-flow'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { ObjectId } from 'mongodb'
 
-export class CashFlowMongoRepository implements AddCashFlowRepository {
+export class CashFlowMongoRepository implements AddCashFlowRepository, LoadCashFlowRepository {
   async add (cashData: AddCashFlowParams): Promise<void> {
     const cashFlowCollection = await MongoHelper.getCollection('cashflows')
     const { categoria, ...cashDataSemCategoria } = cashData
     const { name } = categoria
     await cashFlowCollection.insertOne({ ...cashDataSemCategoria, categoria: { id: new ObjectId(), name } })
+  }
+
+  async loadAll (): Promise<CashFlowModel[]> {
+    const cashFlowCollection = await MongoHelper.getCollection('cashflows')
+    const cashFlows = await cashFlowCollection.find().toArray()
+    return cashFlows
   }
 }
