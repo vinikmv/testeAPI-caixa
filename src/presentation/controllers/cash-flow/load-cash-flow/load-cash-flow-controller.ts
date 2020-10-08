c95f2f3b65ac78ca82ab07a8ef1a331c92561874
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-08 14:23:12 -0300

    feat: ensure LoadCashFlowController returns 500 if LoadCashFlow throws

--- src/presentation/controllers/cash-flow/load-cash-flow/load-cash-flow-controller.ts
+++ src/presentation/controllers/cash-flow/load-cash-flow/load-cash-flow-controller.ts
@@ -1,11 +1,15 @@
-import { ok } from '@/presentation/helpers/http/http-helper'
+import { ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadCashFlow } from './load-cash-flow-protocols'
 
 export class LoadCashFlowController implements Controller {
   constructor (private readonly loadCashFlow: LoadCashFlow) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const cashFlow = await this.loadCashFlow.load()
-    return ok(cashFlow)
+    try {
+      const cashFlow = await this.loadCashFlow.load()
+      return ok(cashFlow)
+    } catch (err) {
+      return serverError(err)
+    }
   }
 }
