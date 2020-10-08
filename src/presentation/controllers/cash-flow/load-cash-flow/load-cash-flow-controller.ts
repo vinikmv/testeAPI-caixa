de21359888d02f8c368510a3611b977db48ec8f3
Vinicius Vicente <viniciuskmv@gmail.com>
2020-10-08 14:18:28 -0300

    feat: ensure LoadCashFlowController returns cashFlow on success

--- src/presentation/controllers/cash-flow/load-cash-flow/load-cash-flow-controller.ts
+++ src/presentation/controllers/cash-flow/load-cash-flow/load-cash-flow-controller.ts
@@ -1,10 +1,11 @@
+import { ok } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadCashFlow } from './load-cash-flow-protocols'
 
 export class LoadCashFlowController implements Controller {
   constructor (private readonly loadCashFlow: LoadCashFlow) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadCashFlow.load()
-    return null
+    const cashFlow = await this.loadCashFlow.load()
+    return ok(cashFlow)
   }
 }
